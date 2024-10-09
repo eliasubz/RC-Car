023fc18e56deaa0c75cc4edf4368867411ef3c03
eliasubz <eliasls2002@yahoo.de>
2024-10-09 18:26:51 +0000

    update was aúpdated to taking in the acceleration instead of speed

--- motor.py
+++ motor.py
@@ -18,7 +18,7 @@ class Motors:
     """
 
 
-    def __init__(self, timesteps):
+    def __init__(self):
         self.l_motor, self.r_motor = self.get_motor()
         self.l_rotor = RotaryEncoder(4, 17, max_steps=0)
         self.r_rotor = RotaryEncoder(5, 13, max_steps=0)
@@ -27,8 +27,6 @@ class Motors:
         self.tstart = time.perf_counter()
         self.tprev = 0
         self.tcurr = 0
-        self.tsteps = timesteps
-        self.tstart = time.perf_counter()
         self.lprevstep = 0
         self.rprevstep = 0
 
@@ -44,34 +42,38 @@ class Motors:
 
         motorL= motor.DCMotor(pwmina1, pwmina2)
         motorR = motor.DCMotor(pwminb1, pwminb2)
-        motorL.throttle = 1
-
+        motorL.throttle = 0
+        motorR.throttle = 0
         return motorL, motorR
 
 
     # calculates the steps since the last timestep
     def get_steps_p_sam(self):
-        self.tcurr = time.perf_counter - self.tstart
+        self.tcurr = time.perf_counter() - self.tstart
+        print("current time ", self.tcurr )
         l_speed = (self.l_rotor.steps - self.lprevstep) / (self.tcurr - self.tprev)
         r_speed = (self.r_rotor.steps - self.rprevstep) / (self.tcurr - self.tprev)
-
+        self.lprevstep = self.l_rotor.steps
+        self.rprevstep = self.r_rotor.steps
+        self.tprev = self.tcurr
         return l_speed, r_speed
 
 
     def update(self,l_motor_power, r_motor_power):
-        self.l_motor.throttle = l_motor_power
-        self.r_motor.throttle = r_motor_power
+        # Calculate and clamp the left motor throttle
+        self.l_motor.throttle = max(-1, min(self.l_motor.throttle + l_motor_power, 1))
+
+        self.r_motor.throttle = max(-1, min(self.r_motor.throttle + r_motor_power, 1))
 
         # Time passes
         time.sleep(0.2)
 
         return self.get_steps_p_sam()
-    
 
     def setup(self, l_rot_p_sam_goal, r_rot_p_sam_goal):
-        kp = 0.25
-        ki = 0.01
-        kd = 0
+        kp = 0.00005
+        ki = 0.001
+        kd = 0.001
         l_pid = PID(kp, ki, kd, setpoint=l_rot_p_sam_goal)
         l_pid.output_limits = (-1, 1)
 
@@ -85,42 +87,36 @@ if __name__ == '__main__':
     motors = Motors()
 
     l_rot, r_rot = motors.get_steps_p_sam()
-    kp = 0.25
-    ki = 0.01
-    kd = 0
-    l_pid = PID(kp, ki, kd, setpoint=20)
+    kp = 0.001
+    ki = 0.00
+    kd = 0.00
+    l_goal_speed = 500
+    r_goal_speed = 500
+    l_pid = PID(kp, ki, kd, setpoint=l_goal_speed)
     l_pid.output_limits = (-1, 1)
 
 
-    r_pid = PID(kp, ki, kd, setpoint=20)
+    r_pid = PID(kp, ki, kd, setpoint=r_goal_speed)
     r_pid.output_limits = (-1, 1)
 
-    start_time = time.time()
-    last_time = start_time
+    l_speed,r_speed = motors.get_steps_p_sam()
 
     # Keep track of values for plotting
     setpoint, y, x = [], [], []
+    start_time = time.time()
+    while time.time() - start_time < 20:
 
-    while time.time() - start_time < 10:
-
-
-        current_time = time.time()
-        dt = current_time - last_time
-
-        l_power = l_pid(l_rot)
-        r_power = r_pid(r_rot)
-        print("r_rot: ", r_rot)
-        l_rot, r_rot = motors.update(l_power, r_power)
-        print("l rotations ", l_rot, " r_rotations " , r_rot)
+        l_power = l_pid(l_speed)
+        r_power = r_pid(r_speed)
+        l_speed, r_speed = motors.update(l_power, r_power)
+        print("l_s ", l_speed, " r_s " , r_speed, " l_p ", l_power, " r_p ", r_power)
 
         x += [r_rot]
         y += [r_power]
         setpoint += [l_pid.setpoint]
 
-        if current_time - start_time > 1:
-            l_pid.setpoint = 20
 
-        last_time = current_time
+
 
 print("x \n", x)
 print("y \n", y)
