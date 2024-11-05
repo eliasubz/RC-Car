d7f64040fc624f14bfef1711c1725062dd04f90f
Maix Linux <eliasls2002@yahoo.de>
2024-11-05 16:55:57 +0000

    more oower

--- server_with_motor_control.py
+++ server_with_motor_control.py
@@ -112,7 +112,7 @@ def old_adjust_alignment(rgb, motors):
             "We see red GO Right",
         )
         motors.r_motor.throttle = 0
-        motors.l_motor.throttle = 0.1
+        motors.l_motor.throttle = 0.2
         while True:
             r, g, b = rgb.sensor.color_rgb_bytes
             if r < g + b:
@@ -123,7 +123,7 @@ def old_adjust_alignment(rgb, motors):
     elif b > r + g or b > 15:
         print("We see blue Go LEFT")
         motors.l_motor.throttle = 0
-        motors.r_motor.throttle = 0.1
+        motors.r_motor.throttle = 0.2
         while True:
             r, g, b = rgb.sensor.color_rgb_bytes
             if b < r + g:
@@ -263,6 +263,7 @@ def drive_car(command, motors):
     
     elif command == "c":
         change += 1
+        command = ""
 
 
     elif command == "r":
